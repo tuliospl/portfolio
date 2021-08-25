d28cf8c0b87cf5bd2ebb052e3085140dbb2b079d
Tulio Sploradori <tuliospl@gmail.com>
2021-08-25 13:32:47 -0300

    Implementacao do Header

--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ImgPerfil from '../images/Perfil.jpg';
+import PtBrIcon from '../images/pt-br.jpg';
+import English from '../images/english.jpeg';
+import './style/Header.css';
 
 export default function Header() {
   return (
-    <div>
-      <h1>Bem vindo ao meu portifolio</h1>
-      <button>Language</button>
+    <div className="container-header">
+      <div>
+        <img className="language" src={PtBrIcon} alt="Portugues" />
+        <img className="language" src={English} alt="English" />
+      </div>
+
+      <h1>Túlio Dantas Sploradori</h1>
       <img alt="imagem de perfil" src={ImgPerfil} width="150px" height="200px"  />
     </div>
   )
